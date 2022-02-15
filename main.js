79c0df5bbc73c8b8675b2f33c543125c89655206
Netson <netsoncavina@gmail.com>
2022-02-15 19:33:39 -0300

    added preLoad function to load image and gitignore

---
+++ main.js
@@ -0,0 +1,11 @@
+const density = "Ñ@#$9876543210?!abc;:+=-,._";
+function preLoad() {
+  mateo = loadImage("Mateo.png");
+}
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
