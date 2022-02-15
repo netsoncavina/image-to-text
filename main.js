bc6621f563259aff3ba2f86fc94a8d3ac11762b9
Netson <netsoncavina@gmail.com>
2022-02-15 19:39:47 -0300

    added the image and the pixelation code

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const density = "Ñ@#$9876543210?!abc;:+=-,._";
+let mateo;
 function preLoad() {
   mateo = loadImage("Mateo.png");
 }
 function setup() {
   createCanvas(400, 400);
+  preLoad();
 }
 
 function draw() {
   background(220);
+  image(mateo, 0, 0, width, height);
+
+  for (let i = 0; i < mateo.width; i++) {
+    for (let j = 0; j < mateo.height; j++) {
+      const pixelIndex = (i + j * mateo.width) * 4;
+      const r = mateo.pixels[pixelIndex + 0];
+      const g = mateo.pixels[pixelIndex + 1];
+      const b = mateo.pixels[pixelIndex + 2];
+    }
+  }
 }
